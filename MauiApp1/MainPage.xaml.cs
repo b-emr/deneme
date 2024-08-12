405ba6ceb705e3e23666321680948aebec39d78d
b-emr <befe22@ku.edu.tr>
2024-08-12 15:33:53 +0300

    commitlendi

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+	private void NewOnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			NewBtn.Text = $"Clicked -{count} time do not click";
+		else
+			NewBtn.Text = $"Clicked -{count} times";
+
+		SemanticScreenReader.Announce(NewBtn.Text);
+	}
+}
+
